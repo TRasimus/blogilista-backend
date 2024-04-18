098e368b8b5157dbb617a19248f89abf4653cd17
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2024-04-18 21:11:16 +0300

    Tehtävä 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
+const logger = require('./logger')
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  return likes.reduce((acc, sum) => acc + sum, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
